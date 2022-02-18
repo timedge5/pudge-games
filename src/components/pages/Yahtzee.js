28c9beffcf6fd73cfdee00ef7fd0d089e93c6648
Tim Edge <timedge5@gmail.com>
2022-02-17 22:09:42 -0800

    Reconfigured scorecard with selectors for set values

--- src/components/pages/Yahtzee.js
+++ src/components/pages/Yahtzee.js
@@ -7,7 +7,7 @@ export const Yahtzee = () => {
   const [lower, setLower] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
 
   // arrays for labels
-  const upperLabels = ["1", "2", "3", "4", "5", "6"];
+  const upperLabels = [1, 2, 3, 4, 5, 6];
   const lowerLabels = [
     "3 of a kind",
     "4 of a kind",
@@ -49,8 +49,12 @@ export const Yahtzee = () => {
 
   const handleClear = () => {
     let inputs = document.querySelectorAll("input");
+    let selects = document.querySelectorAll("select");
     inputs.forEach((input) => {
       input.value = "";
+    });
+    selects.forEach((select) => {
+      select.value = "none";
     });
     setUpper([0, 0, 0, 0, 0, 0]);
     setLower([0, 0, 0, 0, 0, 0, 0, 0]);
@@ -59,66 +63,158 @@ export const Yahtzee = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-blue-200 pt-2 relative md:px-32 lg:px-96 md:pt-5">
-      <h1 className="uppercase font-light text-center mb-3 md:mb-5">yahtzee</h1>
+    <div className="flex flex-col h-screen bg-gray-200 pt-2 relative md:px-32 lg:px-96 md:pt-5">
+      <h1 className="uppercase font-light text-2xl text-center mb-3 md:mb-5">yahtzee</h1>
       <div className="grid grid-cols-2 gap-2 w-3/4 mx-auto mb-2">
         {upperLabels.map((label, i) => (
           <div className="flex" key={i}>
             <p className="p-1 mr-2">{label}</p>
-            <input
-              type="number"
-              className="p-1 w-1/2 mx-auto input"
+            <select
+              className="px-2 cursor-pointer w-1/2 bg-white"
               onChange={(e) => handleUpperInput(e, i)}
-            />
+            >
+              <option value="none" selected disabled hidden>
+                -
+              </option>
+              <option value={0}>0</option>
+              <option value={label}>{label}</option>
+              <option value={label * 2}>{label * 2}</option>
+              <option value={label * 3}>{label * 3}</option>
+              <option value={label * 4}>{label * 4}</option>
+              <option value={label * 5}>{label * 5}</option>
+            </select>
           </div>
         ))}
       </div>
-      <div className="flex w-3/4 mx-auto mt-2 pt-2 border-t-2 border-gray-400">
+      {/* upper score */}
+      <div className="flex w-3/4 mx-auto mt-1 pt-2 border-t-2 border-gray-400">
         <p className="w-1/2">Upper</p>
-        <p className="px-2 bg-gray-100 border-2 border-bg-400">
+        <p className="px-4 bg-gray-100 border-2 border-bg-400">
           {upperSubtotal}
         </p>
       </div>
       <div className="flex mx-auto w-3/4 my-1">
         <p className="w-1/2">Bonus</p>
-        <p className="px-2 bg-gray-100 border-2 border-bg-400">
+        <p className="px-4 bg-gray-100 border-2 border-bg-400">
           {upperSubtotal > 62 ? 35 : 0}
         </p>
       </div>
       <div className="flex mx-auto w-3/4 pb-2 mb-2 border-b-2 border-gray-400">
         <p className="w-1/2">Upper Total</p>
-        <p className="px-2 bg-gray-100 border-2 border-bg-400">
+        <p className="px-4 bg-gray-100 border-2 border-bg-400">
           {upperSubtotal > 62 ? upperSubtotal + 35 : upperSubtotal}
         </p>
       </div>
+      {/* lower inputs */}
       <div className="mx-auto w-3/4">
-        {lowerLabels.map((label, i) => (
-          <div className="flex mb-1" key={i}>
-            <p className="p-1 mr-2">{label}</p>
-            <input
-              type="number"
-              className="p-1 w-1/4 mx-auto input mr-0"
-              onChange={(e) => handleLowerInput(e, i)}
-            />
-          </div>
-        ))}
+        <div className="flex mb-1">
+          <p className="p-1 mr-2">3 of a kind</p>
+          <input
+            type="number"
+            className="p-1 w-1/4 mx-auto input mr-0"
+            onChange={(e) => handleLowerInput(e, 1)}
+          />
+        </div>
+        <div className="flex mb-1">
+          <p className="p-1 mr-2">4 of a kind</p>
+          <input
+            type="number"
+            className="p-1 w-1/4 mx-auto input mr-0"
+            onChange={(e) => handleLowerInput(e, 2)}
+          />
+        </div>
+        <div className="flex justify-between mb-1">
+          <p className="p-1 mr-2">Full House</p>
+          <select
+            className="px-2 cursor-pointer bg-white w-1/4"
+            onChange={(e) => handleLowerInput(e, 3)}
+          >
+            <option value="none" selected disabled hidden>
+              0
+            </option>
+            <option value={0}>-</option>
+            <option value={25}>25</option>
+          </select>
+        </div>
+        <div className="flex justify-between mb-1">
+          <p className="p-1 mr-2">Small Straight</p>
+          <select
+            className="px-2 cursor-pointer bg-white w-1/4"
+            onChange={(e) => handleLowerInput(e, 4)}
+          >
+            <option value="none" selected disabled hidden>
+              0
+            </option>
+            <option value={0}>-</option>
+            <option value={30}>30</option>
+          </select>
+        </div>
+        <div className="flex justify-between mb-1">
+          <p className="p-1 mr-2">Large Straight</p>
+          <select
+            className="px-2 cursor-pointer bg-white w-1/4"
+            onChange={(e) => handleLowerInput(e, 5)}
+          >
+            <option value="none" selected disabled hidden>
+              0
+            </option>
+            <option value={0}>-</option>
+            <option value={40}>40</option>
+          </select>
+        </div>
+        <div className="flex justify-between mb-1">
+          <p className="p-1 mr-2">Yahtzee</p>
+          <select
+            className="px-2 cursor-pointer bg-white w-1/4"
+            onChange={(e) => handleLowerInput(e, 6)}
+          >
+            <option value="none" selected disabled hidden>
+              0
+            </option>
+            <option value={0}>-</option>
+            <option value={50}>50</option>
+          </select>
+        </div>
+        <div className="flex mb-1">
+          <p className="p-1 mr-2">Chance</p>
+          <input
+            type="number"
+            className="p-1 w-1/4 mx-auto input mr-0"
+            onChange={(e) => handleLowerInput(e, 7)}
+          />
+        </div>
+        <div className="flex justify-between mb-1">
+          <p className="p-1 mr-2">Bonus</p>
+          <select
+            className="px-2 cursor-pointer bg-white w-1/4"
+            onChange={(e) => handleLowerInput(e, 6)}
+          >
+            <option value="none" selected disabled hidden>
+              -
+            </option>
+            <option value={100}>100</option>
+            <option value={200}>200</option>
+            <option value={300}>300</option>
+          </select>
+        </div>
       </div>
+      {/* scoring */}
       <div className="flex mx-auto w-3/4 mb-1 mt-2 pt-2 border-t-2 border-gray-400">
         <p className="w-1/2">Lower</p>
-        <p className="ml-2 px-2 bg-gray-100 border-2 border-bg-400">
+        <p className="ml-2 px-4 bg-gray-100 border-2 border-bg-400">
           {lowerSubtotal}
         </p>
       </div>
       <div className="flex mx-auto w-3/4">
         <p className="w-1/2">Total</p>
-        <p className="ml-2 px-2 bg-gray-100 border-2 border-bg-400">
+        <p className="ml-2 px-4 bg-gray-100 border-2 border-bg-400">
           {upperSubtotal > 62
             ? upperSubtotal + 35 + lowerSubtotal
             : upperSubtotal + lowerSubtotal}
         </p>
       </div>
       <button
-        className="p-1 uppercase text-xs bg-gray-200 border-2 border-gray-400 absolute top-2 right-2 hover:bg-gray-100 md:top-5 md:right-5 lg:right-96"
+        className="p-2 uppercase text-xs text-white bg-red-400 absolute top-2 right-2 hover:scale-110 md:top-5 md:right-5 lg:right-96"
         onClick={() => handleClear()}
       >
         clear
